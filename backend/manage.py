e60974bceab676bf69661184aad3893cf504634a
wncfe <alekseip2002@gmail.com>
2026-06-15 00:45:02 +0500

    подсказки снова работают

--- backend/manage.py
+++ backend/manage.py
@@ -6,6 +6,8 @@ import sys
 
 def main():
     """Run administrative tasks."""
+    from dotenv import load_dotenv
+    load_dotenv()
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
     try:
         from django.core.management import execute_from_command_line
